8e0b535eec513ff16a1a56239c1fb3d6b0a7bd95
Alexander Jacob <alex.r.jacob8@gmail.com>
2026-04-02 14:49:19 +1100

    Add main R code file to directory

---
+++ stage_2.R
@@ -0,0 +1,10 @@
+# =========================================================
+
+# Script: Segment 2.r
+
+# Author: Alexander Jacob
+
+# Created:31-03-2026
+
+# Purpose: Compare regression models & create tree. 
+# =========================================================
